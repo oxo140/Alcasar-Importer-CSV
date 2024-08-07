66a84a7203ce3d007015ae5c98d086a6320a2e41
william <136432112+oxo140@users.noreply.github.com>
2024-08-07 15:59:59 +0200

    Update upload.php
    ajout de l'option de supprimer a partir du CSV Source importer préalablement

--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 <?php
 // Configuration de la base de données
-$servername = "localhost"; // ou votre serveur de base de données
+$servername = "localhost";
 $username = "radius";
 $password = "MDP BDD /root/ALCASAR-passwords.txt";
 $dbname = "radius";
@@ -34,47 +34,69 @@ if (isset($_FILES['fileToUpload'])) {
             // Ignorer la ligne d'en-tête
             fgetcsv($handle, 1000, ",");
 
-            // Lire les données du CSV
-            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                // Récupérer les valeurs du CSV
-                $username = $data[1];
-                $attribute = $data[2];
-                $value = generate_sha256_crypt($data[4]); // Chiffrement SHA-256 de la valeur
-                $name = $data[5];
-                $mail = $data[6];
-                $department = $data[7];
-                $workphone = $data[8];
-                $homephone = $data[9];
-                $mobile = $data[10];
+            if (isset($_POST['submit'])) {
+                // Lire les données du CSV pour ajout
+                while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                    $username = $data[1];
+                    $attribute = $data[2];
+                    $value = generate_sha256_crypt($data[4]); // Chiffrement SHA-256 de la valeur
+                    $name = $data[5];
+                    $mail = $data[6];
+                    $department = $data[7];
+                    $workphone = $data[8];
+                    $homephone = $data[9];
+                    $mobile = $data[10];
 
-                // Insérer dans radcheck et obtenir l'id auto-incrémenté
-                $radcheck_sql = $conn->prepare("INSERT INTO radcheck (username, attribute, value) VALUES (?, ?, ?)");
-                $radcheck_sql->bind_param("sss", $username, $attribute, $value);
-                if ($radcheck_sql->execute()) {
-                    $id = $conn->insert_id;
+                    // Insérer dans radcheck et obtenir l'id auto-incrémenté
+                    $radcheck_sql = $conn->prepare("INSERT INTO radcheck (username, attribute, value) VALUES (?, ?, ?)");
+                    $radcheck_sql->bind_param("sss", $username, $attribute, $value);
+                    if ($radcheck_sql->execute()) {
+                        $id = $conn->insert_id;
 
-                    // Insérer dans userinfo avec l'id obtenu
-                    $userinfo_sql = $conn->prepare("INSERT INTO userinfo (id, username, name, mail, department, workphone, homephone, mobile) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-                    $userinfo_sql->bind_param("isssssss", $id, $username, $name, $mail, $department, $workphone, $homephone, $mobile);
-                    if (!$userinfo_sql->execute()) {
-                        echo "Error inserting into userinfo: " . $userinfo_sql->error . "<br>";
+                        // Insérer dans userinfo avec l'id obtenu
+                        $userinfo_sql = $conn->prepare("INSERT INTO userinfo (id, username, name, mail, department, workphone, homephone, mobile) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+                        $userinfo_sql->bind_param("isssssss", $id, $username, $name, $mail, $department, $workphone, $homephone, $mobile);
+                        if (!$userinfo_sql->execute()) {
+                            echo "Error inserting into userinfo: " . $userinfo_sql->error . "<br>";
+                        }
+                        $userinfo_sql->close();
+                    } else {
+                        echo "Error inserting into radcheck: " . $radcheck_sql->error . "<br>";
                     }
-                    $userinfo_sql->close();
-                } else {
-                    echo "Error inserting into radcheck: " . $radcheck_sql->error . "<br>";
+                    $radcheck_sql->close();
                 }
-                $radcheck_sql->close();
+                echo "Data has been imported successfully.";
+            } elseif (isset($_POST['delete'])) {
+                // Lire les données du CSV pour suppression
+                while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                    $username = $data[1];
+
+                    // Supprimer de radcheck
+                    $delete_radcheck_sql = $conn->prepare("DELETE FROM radcheck WHERE username = ?");
+                    $delete_radcheck_sql->bind_param("s", $username);
+                    if (!$delete_radcheck_sql->execute()) {
+                        echo "Error deleting from radcheck: " . $delete_radcheck_sql->error . "<br>";
+                    }
+                    $delete_radcheck_sql->close();
+
+                    // Supprimer de userinfo
+                    $delete_userinfo_sql = $conn->prepare("DELETE FROM userinfo WHERE username = ?");
+                    $delete_userinfo_sql->bind_param("s", $username);
+                    if (!$delete_userinfo_sql->execute()) {
+                        echo "Error deleting from userinfo: " . $delete_userinfo_sql->error . "<br>";
+                    }
+                    $delete_userinfo_sql->close();
+                }
+                echo "Users have been deleted successfully.";
             }
 
-            // Fermer la connexion à la base de données
-            $conn->close();
-
             fclose($handle);
-
-            echo "Data has been imported successfully.";
         } else {
             echo "Cannot open the file.";
         }
+
+        // Fermer la connexion à la base de données
+        $conn->close();
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
