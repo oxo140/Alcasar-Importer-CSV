6ef578bd4c31fa4bb62a954b564834a7a31c9014
william <136432112+oxo140@users.noreply.github.com>
2024-08-08 12:12:50 +0200

    Update index.php
    ajout du bouton extraire la base de données

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
         input[type="file"] {
             margin-bottom: 10px;
         }
-        input[type="submit"] {
+        input[type="submit"], button {
             background-color: #4CAF50;
             color: white;
             border: none;
@@ -38,7 +38,7 @@
             font-size: 16px;
             margin-bottom: 10px;
         }
-        input[type="submit"]:hover {
+        input[type="submit"]:hover, button:hover {
             background-color: #45a049;
         }
         input[type="submit"].delete {
@@ -47,16 +47,25 @@
         input[type="submit"].delete:hover {
             background-color: #e53935;
         }
+        button {
+            background-color: #2196F3;
+        }
+        button:hover {
+            background-color: #1e88e5;
+        }
     </style>
 </head>
 <body>
     <div class="container">
         <h2>Importer votre fichier CSV</h2>
         <form action="upload.php" method="post" enctype="multipart/form-data">
-            Choisir votre fichier CSV a importer:
+            Choisir votre fichier CSV à importer:
             <input type="file" name="fileToUpload" id="fileToUpload">
             <input type="submit" value="Upload CSV" name="submit">
             <input type="submit" value="Reverse CSV" name="delete" class="delete">
+        </form>
+        <form action="backup.php" method="post">
+            <button type="submit" name="backup">Extraire la base de données</button>
         </form>
     </div>
 </body>
