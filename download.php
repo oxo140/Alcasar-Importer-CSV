24929c7309e7edd8fce90496c1cedc19c254587b
william <136432112+oxo140@users.noreply.github.com>
2024-08-09 10:35:28 +0200

    Update download.php
    mot de passe

--- download.php
+++ download.php
@@ -1,4 +1,14 @@
 <?php
+session_start();
+
+// Vérifie si l'utilisateur est authentifié
+if (!isset($_SESSION['authenticated'])) {
+    header('Location: login.php');
+    exit;
+}
+?>
+
+<?php
 // Chemin vers le répertoire à télécharger
 $directory = '/var/www/html/csv/apache';
 
